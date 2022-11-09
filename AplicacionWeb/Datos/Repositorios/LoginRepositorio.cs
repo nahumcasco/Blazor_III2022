eb25dd9ad97e0a1a9ba78aef7214d3bcd0aadc77
Nahum Casco <nahumcasco@gmail.com>
2022-11-08 19:51:15 -0600

    PRIMER COMIT
    PRIMER COMIT

---
+++ AplicacionWeb/Datos/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Datos.Interfaces;
+using Modelos;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Datos.Repositorios
+{
+    public class LoginRepositorio : ILoginRepositorio
+    {
+        private string CadenaConexon;
+
+        public LoginRepositorio(string _cadenaConexion)
+        {
+            CadenaConexon = _cadenaConexion;
+        }
+
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexon);
+        }
+
+        public async Task<bool> ValidarUsuario(Login login)
+        {
+            bool valido = false;
+            try
+            {
+                using MySqlConnection conexion = Conexion();
+                await conexion.OpenAsync();
+                string sql = "SELECT 1 FROM usuario WHERE Codigo = @Codigo AND Clave = @Clave;";
+                valido = await conexion.ExecuteScalarAsync<bool>(sql, new { login.Usuario, login.Clave });
+            }
+            catch (Exception ex)
+            {
+            }
+            return valido;
+        }
+    }
+}
