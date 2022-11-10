ab814e277fb95dffd19808eb8f7bd2836c995706
Nahum Casco <nahumcasco@gmail.com>
2022-11-09 20:54:45 -0600

    Login
    Se trabajo en el método de autenticación y autorización

--- AplicacionWeb/Blazor/Program.cs
+++ AplicacionWeb/Blazor/Program.cs
@@ -1,11 +1,19 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Blazor;
+using Blazor.Interfaces;
+using Blazor.Servicios;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+Config cadenaConexion = new Config(builder.Configuration.GetConnectionString("MySQL"));
+builder.Services.AddSingleton(cadenaConexion);
+
+builder.Services.AddScoped<ILoginServicio, LoginServicio>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
 var app = builder.Build();
 
@@ -20,6 +28,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
---
+++ AplicacionWeb/Datos/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Modelos;
+
+namespace Datos.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<Usuario> GetPorCodigo(string codigo);
+        Task<bool> Nuevo(Usuario usuario);
+        Task<bool> Actualizar(Usuario usuario);
+        Task<bool> Eliminar(string codigo);
+        Task<IEnumerable<Usuario>> GetLista();
+    }
+}
